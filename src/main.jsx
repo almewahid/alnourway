eca99669235e2206c2b965e68905194b59be44dc
almewahid <osakr100@gmail.com>
2025-11-27 14:38:34 +0200

    إصلاح ملف main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,18 @@
-<<<<<<< HEAD
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App.jsx';
 import '@/index.css';
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
-import { AuthProvider } from '@/context/AuthContext'
->>>>>>> 3a77d1ed7952bf6ff3f0b4d36b02442c9a2b7710
+import { AuthProvider } from '@/context/AuthContext';
 
 // Render Application
 ReactDOM.createRoot(document.getElementById('root')).render(
-<<<<<<< HEAD
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
-=======
-  // <React.StrictMode>
-  <AuthProvider>
-    <App />
-  </AuthProvider>
-  // </React.StrictMode>,
-)
->>>>>>> 3a77d1ed7952bf6ff3f0b4d36b02442c9a2b7710
 
 // Base44 Sandbox HMR Bridge (Keep It!)
 if (import.meta.hot) {
@@ -36,8 +22,4 @@ if (import.meta.hot) {
   import.meta.hot.on('vite:afterUpdate', () => {
     window.parent?.postMessage({ type: 'sandbox:afterUpdate' }, '*');
   });
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 3a77d1ed7952bf6ff3f0b4d36b02442c9a2b7710
