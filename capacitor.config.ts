a576da581ac6827eac7f8984e7e1029d19dc8d67
almewahid <osakr100@gmail.com>
2026-01-26 14:40:50 +0200

    Update appName to English

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.osama.alnoorway',
-  appName: 'طريق النور',
+  appName: 'Alnoorway',
   webDir: 'dist',
   server: {
     url: 'https://www.alnoorway.com',
